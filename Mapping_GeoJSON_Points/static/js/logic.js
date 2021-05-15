e1b74d352fd98b5e4bf96353f986f3eca0ffde6f
Sindie Castro <Sindie Castro>
2021-05-15 09:56:53 -0500

    adding edits

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -1,3 +1,6 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -23,8 +26,9 @@ let map = L.map('mapid', {
 })
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
+
 // Accessing the airport GeoJSON URL
-let airportData = "https://raw.githubusercontent.com/Sindie.Castro/Mapping_Earthquakes/Mapping_GeoJSON_Points/majorAirports.json"
+let airportData = "https://raw.githubusercontent.com/SindieCastro/Mapping_Earthquakes/Mapping_GeoJSON_Points/majorAirports.json"
 
 d3.json(airportData).then(function(data) {
     L.geoJson(data, {
